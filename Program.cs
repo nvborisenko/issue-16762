7f0709a0d9703f8ff41dbbdc9b1a2a54bfadda2d
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-01-09 15:17:01 +0300

    Proj

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+OpenQA.Selenium.Internal.Logging.Log.SetLevel(OpenQA.Selenium.Internal.Logging.LogEventLevel.Trace);
+
+using var driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/upload");
+var fileInput = driver.FindElement(By.CssSelector("input[type='file']"));
+string filePath = Path.GetFullPath("Manuscript_Unicode_€.docx");
+fileInput.SendKeys(filePath);
